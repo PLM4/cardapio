f415157270893c4ebb7adf8e11ce49d4ce86b135
PLM4 <pedrolucasmelo051@gmail.com>
2025-09-26 22:18:45 -0300

    feat: cria ligação do back com o front por meio do axios e do react-query

--- cardapio-frontend/src/App.tsx
+++ cardapio-frontend/src/App.tsx
@@ -1,13 +1,22 @@
 import "./App.css";
+import { Card } from "./components/card/index";
+import { useFoodData } from "./hooks/useFoodData";
 
 function App() {
+  const { data } = useFoodData();
   return (
-    <>
-      <div className="container">
-        <h1>Cardápio</h1>
-        <div className="card-grid"></div>
+    <div className="container">
+      <h1>Cardápio</h1>
+      <div className="card-grid">
+        {data?.map((foodData) => (
+          <Card
+            image={foodData.image}
+            price={foodData.price}
+            title={foodData.title}
+          />
+        ))}
       </div>
-    </>
+    </div>
   );
 }
 
